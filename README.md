38db1a6591167b44c2aa93695129929f2d71eeea
John Shearer <john@ultranaut.com>
2015-02-27 12:01:37 -0500

    Move notes out of code and into README

--- README.md
+++ README.md
@@ -3,3 +3,42 @@
 ---
 
 Calculating mean stress effects on strain-life fatigue behavior.
+
+## Some random notes
+  * [Cyclic Deformation & Strain-life (ε-N) Approach]( https://www.efatigue.com/training/Chapter_5.pdf )
+  * [Fatigue Life Program Using Strain-life Methods](http://www.dtic.mil/cgi-bin/GetTRDoc?AD=ADA267310)
+
+From FLP, pg. 54:
+
+    '============================  EQUATIONS2  ============================
+    '  subroutine to evaluate Morrow's Strain-Life equation
+    '======================================================================
+    SUB EQUATIONS2
+    REM $DYNAMIC
+
+    DIM Yprime AS DOUBLE, Y AS DOUBLE, loopcount AS LONG
+
+    NNf(i) = 10000
+    loopcount = 0
+
+    DO
+        loopcount = loopcount + 1
+        Y = -deltaeps(i) / 2 + ((sigff - sigO(i)) / E) * (2 * NNf(i)) ^ b + epsff *
+                (2 * NNf(i)) ^ c
+        IF ABS(Y) > .0000000001# THEN
+            Yprime = (b * (sigff - sigO(i)) / E) * (2 ^ b) * (NNf(i)) ^ (b- 1) + c
+                * epsff * (2 ^ c) * (NNf(i)) ^ (c - 1)
+            IF (Y / Yprime < NNf(i)) THEN
+                  NNf(i) = NNf(i) - Y / Yprime
+              ELSE
+                  NNf(i) = NNf(i) / 2
+              END IF
+          END IF
+        LOOP UNTIL (ABS(Y) <= .0000000001#) OR (NNf(i) > 100000000) OR (loopcount=10000)
+
+        usedlife = usedlife + 1 / NNf(i)
+
+        IF loopcount = 10000 THEN PRINT "*"      'indication of convergence dificulties
+
+        END SUB
+
--- morrow.py
+++ morrow.py
@@ -1,9 +1,4 @@
-"""
-Strain life calculator.
-
-https://www.efatigue.com/training/Chapter_5.pdf
-http://www.dtic.mil/cgi-bin/GetTRDoc?AD=ADA267310
-"""
+"""Strain life calculator."""
 
 def morrow(e_target, sf, b, ef, c, e_modulus, s_mean, e_error=1e-7):
     n = 1
@@ -68,72 +63,3 @@ def test():
 
 def check(e_target, n, sf, b, ef, c, e_modulus, s_mean):
     return ((sf - s_mean) / e_modulus) * (2 * n)**b + ef * (2 * n)**c - e_target
-
-
-"""
-http://www.dtic.mil/cgi-bin/GetTRDoc?AD=ADA267310
-pg. 54
-
-'============================  EQUATIONS2  ============================
-'  subroutine to evaluate Morrow's Strain-Life equation
-'======================================================================
-SUB EQUATIONS2
-REM $DYNAMIC
-
-DIM Yprime AS DOUBLE, Y AS DOUBLE, loopcount AS LONG
-
-NNf(i) = 10000
-loopcount = 0
-
-DO
-    loopcount = loopcount + 1
-    Y = -deltaeps(i) / 2 + ((sigff - sigO(i)) / E) * (2 * NNf(i)) ^ b + epsff *
-             (2 * NNf(i)) ^ c
-    IF ABS(Y) > .0000000001# THEN
-        Yprime = (b * (sigff - sigO(i)) / E) * (2 ^ b) * (NNf(i)) ^ (b- 1) + c
-             * epsff * (2 ^ c) * (NNf(i)) ^ (c - 1)
-        IF (Y / Yprime < NNf(i)) THEN
-              NNf(i) = NNf(i) - Y / Yprime
-          ELSE
-              NNf(i) = NNf(i) / 2
-          END IF
-      END IF
-    LOOP UNTIL (ABS(Y) <= .0000000001#) OR (NNf(i) > 100000000) OR (loopcount=10000)
-
-    usedlife = usedlife + 1 / NNf(i)
-
-    IF loopcount = 10000 THEN PRINT "*"      'indication of convergence dificulties
-
-    END SUB
-"""
-
-
-"""
-let foo = ((sigff - sigO(i)) / E)
-let Ea = deltaeps(i) / 2
-let Ef = epsff
-let N = NNf(i)
-
-Y  =     foo * (2 ^ b) * N ^ b      +     Ef * (2 ^ c) * N ^ c       - Ea
-Y' = b * foo * (2 ^ b) * N ^ (b- 1) + c * Ef * (2 ^ c) * N ^ (c - 1)
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
