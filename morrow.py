1f2b1317e82d60656748839520db47a89c3a73ad
John Shearer <john@ultranaut.com>
2015-02-28 15:24:50 -0500

    More changes.

--- morrow.py
+++ morrow.py
@@ -1,65 +1,79 @@
-"""Strain life calculator."""
+"""Strain-life calculator."""
 
-def morrow(e_target, sf, b, ef, c, e_modulus, s_mean, e_error=1e-7):
-    n = 1
-    sf_e = (sf - s_mean) / e_modulus
+materials = [
+    {
+        'label': 'Austempered ductile iron',
+        'sf':        209732.4442,
+        'b':        -0.108094966,
+        'ef':        0.76015,
+        'c':        -0.7126,
+        'e_modulus': 24800000
+    },
+    {
+        'label': '60-40-18',
+        'sf':        102.1,
+        'b':        -0.0596,
+        'ef':        0.5148,
+        'c':        -0.6827,
+        'e_modulus': 25000
+    },
+]
 
-    i = 1
-
-    while True:
-        i += 1
-        print ('%d: %f' % (i, n))
-        if i > 1000:
-            print 'not converging?'
-            break
-        e = sf_e * (2**b) * n**b + ef * (2**c) * n**c
-        Slope = sf_e * (2**b) * n**(b-1) + ef * (2**c) * n**(c-1)
-        n = n + (e - e_target) / Slope
-
-        if abs(e - e_target) <= e_error:
-            break
-    return n
-
-def morrow2(Ea, sf, b, ef, c, e_modulus, s_mean, e_error=1e-7):
+def calculate(Ea, material, s_mean=None, e_error=1e-7):
     # count loop iterations
     i = 0
 
-    sf_e = (sf - s_mean) / e_modulus
+    sf        = material['sf']
+    b         = material['b']
+    ef        = material['ef']
+    c         = material['c']
+    e_modulus = material['e_modulus']
+
+    if s_mean is not None:
+        sf_e = (sf - s_mean) / e_modulus
+    else:
+        sf_e = sf / e_modulus
     n = 1
 
     while True:
         i += 1
-        print ('%d: %f' % (i, n))
-        if i > 1000:
-            print 'not converging?'
-            break
-        e = sf_e * (2**b) * n**b + ef * (2**c) * n**c - Ea
+        print '%d: %f' % (i, n)
+
+        e = life(n, material, s_mean)
         e_prime = b * sf_e * (2**b) * n**(b-1) + c * ef * (2**c) * n**(c-1)
 
         n0 = n
-        n = n - (e / e_prime)
+        # Newtons method
+        n = n - (e - Ea) / e_prime
+        
         if abs(n0 - n) <= e_error:
             break
+    print '**** %d iterations ****' % (i)
     return n
+
+def life(n, material, s_mean=None):
+    sf        = material['sf']
+    b         = material['b']
+    ef        = material['ef']
+    c         = material['c']
+    e_modulus = material['e_modulus']
+    if s_mean is not None:
+        sf_e = (sf - s_mean) / e_modulus
+    else:
+        sf_e = sf / e_modulus
+
+    return sf_e * (2 * n)**b + ef * (2 * n)**c
 
 
 def test():
+    material = materials[1]
     Ea = 0.001631858
-    sf = 209732.4442
-    b = -0.108094966
-    ef = 0.76015
-    c = -0.7126
-    e_modulus = 24800000
     s_mean = 40265.22776
 
-    n = morrow(Ea, sf, b, ef, c, e_modulus, s_mean)
+    n = calculate(Ea, material)
     print '%f cycles' % (n)
-    print 'check: ', check(Ea, n, sf, b, ef, c, e_modulus, s_mean)
+    print 'check: ', life(n, material) - Ea
 
-    n = morrow2(Ea, sf, b, ef, c, e_modulus, s_mean)
+    n = calculate(Ea, material, s_mean)
     print '%f cycles' % (n)
-    print 'check: ', check(Ea, n, sf, b, ef, c, e_modulus, s_mean)
-
-
-def check(e_target, n, sf, b, ef, c, e_modulus, s_mean):
-    return ((sf - s_mean) / e_modulus) * (2 * n)**b + ef * (2 * n)**c - e_target
+    print 'check: ', life(n, material, s_mean) - Ea
