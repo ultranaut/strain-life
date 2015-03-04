eb10714b7466f86100e05d39e61732ec1d43bea5
John Shearer <john@ultranaut.com>
2015-03-04 08:42:02 -0500

    Calculator is functioning.

--- web/app/scripts/controllers/calculator.js
+++ web/app/scripts/controllers/calculator.js
@@ -1,3 +1,4 @@
+/* jshint camelcase: false */
 'use strict';
 
 /**
@@ -10,61 +11,91 @@
 angular.module('strainLifeApp')
   .controller('CalculatorCtrl', function ($scope) {
     $scope.materials = [
-      { 'label':'Custom...',       'sf': null,        'b': null,         'ef': null,    'c': null,    'eModulus': null     },
-      { 'label':'Ductile iron',    'sf': 209732.4442, 'b': -0.108094966, 'ef': 0.76015, 'c': -0.7126, 'eModulus': 24800000 },
-      { 'label':'SAE 1114 (AlFG)', 'sf': 1207,        'b': -0.097,       'ef': 0.85,    'c': -0.46,   'eModulus': 216      },
-      { 'label':'SAE 1114 (AlFG)', 'sf': 1405,        'b': -0.066,       'ef': 0.88,    'c': -0.51,   'eModulus': 227      },
-      { 'label':'SAE 1114 (NbFG)', 'sf': 999,         'b': -0.096,       'ef': 0.76,    'c': -0.46,   'eModulus': 220      },
-      { 'label':'SAE 1114 (NbFG)', 'sf': 1228,        'b': -0.079,       'ef': 0.77,    'c': -0.50,   'eModulus': 217      },
-      { 'label':'SAE 1114 (VFG)',  'sf': 1087,        'b': -0.102,       'ef': 0.68,    'c': -0.52,   'eModulus': 214      },
-      { 'label':'SAE 1114 (VFG)',  'sf': 1243,        'b': -0.086,       'ef': 0.88,    'c': -0.55,   'eModulus': 215      },
-      { 'label':'SAE 1114 (VFG)',  'sf': 1117,        'b': -0.103,       'ef': 0.64,    'c': -0.58,   'eModulus': 220      },
-      { 'label':'SAE 1038',        'sf': 898,         'b': -0.107,       'ef': 0.77,    'c': -0.48,   'eModulus': 201      },
-      { 'label':'SAE 1038',        'sf': 1051,        'b': -0.098,       'ef': 0.76,    'c': -0.44,   'eModulus': 219      },
-      { 'label':'SAE 1038',        'sf': 1197,        'b': -0.097,       'ef': 1.10,    'c': -0.46,   'eModulus': 219      },
+      { 'label':'Custom...',       'sf': null,        'b': null,         'ef': null,    'c': null,    'e_modulus': null     },
+      { 'label':'Ductile iron',    'sf': 209732.4442, 'b': -0.108094966, 'ef': 0.76015, 'c': -0.7126, 'e_modulus': 24800000 },
+      { 'label':'SAE 1114 (AlFG)', 'sf': 1207,        'b': -0.097,       'ef': 0.85,    'c': -0.464,   'e_modulus': 216      },
+      { 'label':'SAE 1114 (AlFG)', 'sf': 1405,        'b': -0.066,       'ef': 0.88,    'c': -0.514,   'e_modulus': 227      },
+      { 'label':'SAE 1114 (NbFG)', 'sf': 999,         'b': -0.096,       'ef': 0.76,    'c': -0.462,   'e_modulus': 220      },
+      { 'label':'SAE 1114 (NbFG)', 'sf': 1228,        'b': -0.079,       'ef': 0.77,    'c': -0.508,   'e_modulus': 217      },
+      { 'label':'SAE 1114 (VFG)',  'sf': 1087,        'b': -0.102,       'ef': 0.68,    'c': -0.529,   'e_modulus': 214      },
+      { 'label':'SAE 1114 (VFG)',  'sf': 1243,        'b': -0.086,       'ef': 0.88,    'c': -0.555,   'e_modulus': 215      },
+      { 'label':'SAE 1114 (VFG)',  'sf': 1117,        'b': -0.103,       'ef': 0.64,    'c': -0.581,   'e_modulus': 220      },
+      { 'label':'SAE 1038',        'sf': 898,         'b': -0.107,       'ef': 0.77,    'c': -0.481,   'e_modulus': 201      },
+      { 'label':'SAE 1038',        'sf': 1051,        'b': -0.098,       'ef': 0.76,    'c': -0.440,   'e_modulus': 219      },
+      { 'label':'SAE 1038',        'sf': 1197,        'b': -0.097,       'ef': 1.10,    'c': -0.460,   'e_modulus': 219      },
       ];
-    $scope.labels = {
-      'sf':        'Fatigue strength coefficient',
-      'b':         'Fatigue strength exponent',
-      'ef':        'Fatigue ductility coefficient',
-      'c':         'Fatigue ductility exponent',
-      'eModulus': 'Elastic modulus'
-      };
     $scope.currentMaterial = $scope.materials[1];
+    $scope.conditions = {
+      's_mean':          40265.22776,
+      'Ea': 0.001631858
+      };
+    $scope.cycles = null;
     $scope.compute = function () {
-      // def calculate(Ea, material, s_mean=None, e_error=1e-7):
-          // count loop iterations
-          /*
-          var i = 0;
+      $scope.cycles = 0;
 
-          var sf       = $scope.currentMaterial.sf;
-          var b        = $scope.currentMaterial.b;
-          var ef       = $scope.currentMaterial.ef;
-          var c        = $scope.currentMaterial.c;
-          var eModulus = $scope.currentMaterial.eModulus;
-          */
+      console.log($scope.currentMaterial);
+      console.log($scope.conditions);
+      // count loop iterations
+      var i = 0;
+      var sf_e;
 
-          // if s_mean is not None:
-          //     sf_e = (sf - s_mean) / eModulus
-          // else:
-          //     sf_e = sf / eModulus
-          // n = 1
+      var sf        = $scope.currentMaterial.sf;
+      var b         = $scope.currentMaterial.b;
+      var ef        = $scope.currentMaterial.ef;
+      var c         = $scope.currentMaterial.c;
+      var e_modulus = $scope.currentMaterial.e_modulus;
+      var s_mean    = $scope.conditions.s_mean;
+      var Ea        = $scope.conditions.Ea;
+      var e_error   = 1e-7;
 
-          // while True:
-          //     i += 1
-          //     print '%d: %f' % (i, n)
+      if (s_mean !== 0) {
+        sf_e = (sf - s_mean) / e_modulus;
+      }
+      else {
+        sf_e = sf / e_modulus;
+      }
+      var n = 1;
 
-          //     e = life(n, material, s_mean)
-          //     e_prime = b * sf_e * (2**b) * n**(b-1) + c * ef * (2**c) * n**(c-1)
+      while (true) {
+        if (i++ > 100) {
+          console.error('Uh oh.');
+          break;
+        }
+        var e = $scope.life(n, s_mean);
+        console.log(e);
+        var e_prime = b * sf_e * Math.pow(2, b) * Math.pow(n, b-1) +
+                      c * ef * Math.pow(2, c) * Math.pow(n, c-1);
 
-          //     n0 = n
-          //     # Newtons method
-          //     n = n - (e - Ea) / e_prime
+        var n0 = n;
+        // Newtons method
+        n = n - (e - Ea) / e_prime;
 
-          //     if abs(n0 - n) <= e_error:
-          //         break
-          // print '**** %d iterations ****' % (i)
-          // return n
+        if (Math.abs(n0 - n) <= e_error) {
+          break;
+        }
+      // print '**** %d iterations ****' % (i);
+      }
+      $scope.cycles = n;
+    };
+
+    $scope.life = function (n, s_mean) {
+      s_mean = s_mean || null;
+
+      var sf        = $scope.currentMaterial.sf;
+      var b         = $scope.currentMaterial.b;
+      var ef        = $scope.currentMaterial.ef;
+      var c         = $scope.currentMaterial.c;
+      var e_modulus = $scope.currentMaterial.e_modulus;
+      var sf_e;
+
+      if (s_mean !== 0) {
+        sf_e = (sf - s_mean) / e_modulus;
+      }
+      else {
+        sf_e = sf / e_modulus;
+      }
+
+      return sf_e * Math.pow((2 * n), b) + ef * Math.pow((2 * n), c);
     };
 
   });
