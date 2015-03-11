5ce49385fe175070270c06ee900b5faa6e7cf831
John Shearer <john@ultranaut.com>
2015-03-11 15:12:34 -0400

    Regularize materials data variable names

--- web/app/scripts/controllers/calculator.js
+++ web/app/scripts/controllers/calculator.js
@@ -14,76 +14,73 @@ angular.module('strainLifeApp')
       $scope.materials = materials;
       });
     $scope.conditions = {
-      's_mean': 40265.22776,
-      'Ea':     0.001631858
+      'sigma_subm': 40265.22776,
+      'epsilon_suba':     0.001631858
       };
-    $scope.cycles = null;
+    $scope.Nu = null;
     $scope.compute = function () {
-      $scope.cycles = 0;
+      $scope.Nu = 0;
 
-      console.log($scope.currentMaterial);
-      console.log($scope.conditions);
       // count loop iterations
       var i = 0;
       var sf_e;
+      var error_tolerance = 1e-3;
 
-      var sf        = $scope.currentMaterial.sf;
-      var b         = $scope.currentMaterial.b;
-      var ef        = $scope.currentMaterial.ef;
-      var c         = $scope.currentMaterial.c;
-      var e_modulus = $scope.currentMaterial.e_modulus;
-      var s_mean    = $scope.conditions.s_mean;
-      var Ea        = $scope.conditions.Ea;
-      var e_error   = 1e-7;
+      var sigma_subf_prime   = $scope.currentMaterial.sigma_subf_prime;
+      var b                  = $scope.currentMaterial.b;
+      var epsilon_subf_prime = $scope.currentMaterial.epsilon_subf_prime;
+      var c                  = $scope.currentMaterial.c;
+      var Epsilon            = $scope.currentMaterial.Epsilon;
 
-      if (s_mean !== 0) {
-        sf_e = (sf - s_mean) / e_modulus;
+      var sigma_subm         = $scope.conditions.sigma_subm;
+      var epsilon_suba       = $scope.conditions.epsilon_suba;
+
+      if (sigma_subm !== 0) {
+        sf_e = (sigma_subf_prime - sigma_subm) / Epsilon;
       }
       else {
-        sf_e = sf / e_modulus;
+        sf_e = sigma_subf_prime / Epsilon;
       }
-      var n = 1;
+      var Nu = 1;
 
       while (true) {
         if (i++ > 100) {
           console.error('Uh oh.');
           break;
         }
-        var e = $scope.life(n, s_mean);
-        console.log(e);
-        var e_prime = b * sf_e * Math.pow(2, b) * Math.pow(n, b-1) +
-                      c * ef * Math.pow(2, c) * Math.pow(n, c-1);
+        var e = $scope.life(Nu, sigma_subm);
+        // console.log(e);
+        var e_prime = b * sf_e * Math.pow(2, b) * Math.pow(Nu, b-1) +
+                      c * epsilon_subf_prime * Math.pow(2, c) * Math.pow(Nu, c-1);
 
-        var n0 = n;
+        var Nu0 = Nu;
         // Newtons method
-        n = n - (e - Ea) / e_prime;
+        Nu = Nu - (e - epsilon_suba) / e_prime;
 
-        if (Math.abs(n0 - n) <= e_error) {
+        if (Math.abs(Nu0 - Nu) <= error_tolerance) {
           break;
         }
-      // print '**** %d iterations ****' % (i);
       }
-      $scope.cycles = n;
+      $scope.Nu = Nu;
     };
 
-    $scope.life = function (n, s_mean) {
-      s_mean = s_mean || null;
+    $scope.life = function (Nu, sigma_subm) {
+      sigma_subm = sigma_subm || null;
 
-      var sf        = $scope.currentMaterial.sf;
-      var b         = $scope.currentMaterial.b;
-      var ef        = $scope.currentMaterial.ef;
-      var c         = $scope.currentMaterial.c;
-      var e_modulus = $scope.currentMaterial.e_modulus;
+      var sigma_subf_prime   = $scope.currentMaterial.sigma_subf_prime;
+      var b                  = $scope.currentMaterial.b;
+      var epsilon_subf_prime = $scope.currentMaterial.epsilon_subf_prime;
+      var c                  = $scope.currentMaterial.c;
+      var Epsilon            = $scope.currentMaterial.Epsilon;
       var sf_e;
 
-      if (s_mean !== 0) {
-        sf_e = (sf - s_mean) / e_modulus;
+      if (sigma_subm !== 0) {
+        sf_e = (sigma_subf_prime - sigma_subm) / Epsilon;
       }
       else {
-        sf_e = sf / e_modulus;
+        sf_e = sigma_subf_prime / Epsilon;
       }
 
-      return sf_e * Math.pow((2 * n), b) + ef * Math.pow((2 * n), c);
+      return sf_e * Math.pow((2 * Nu), b) + epsilon_subf_prime * Math.pow((2 * Nu), c);
     };
-
   });
